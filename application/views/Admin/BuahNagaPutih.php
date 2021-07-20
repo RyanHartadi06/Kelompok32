626c3bf761c6e4b1cf824a4b63036b34e6e37197
idrisrm <55520214+idrisrm@users.noreply.github.com>
2021-07-20 20:30:20 +0700

    Update BuahNagaPutih.php

--- application/views/Admin/BuahNagaPutih.php
+++ application/views/Admin/BuahNagaPutih.php
@@ -7,20 +7,49 @@
                 </div>
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-lg-5 d-flex align-items-center justify-content-center">
-                            <img class="img-fluid" style="max-width: 25rem" src="<?= base_url('image/buahnaga.jpg') ?>" alt="">
+                        <div class="" style="width: 18rem;">
+                            <img class="card-img-top" src="<?= base_url('image/buahnaga.jpg') ?>" alt="Card image cap">
                         </div>
-                        <div class="col-lg-7 pt-4 pt-lg-0 content align-items-center">
+                        <div class="col-lg-8 pt-4 pt-lg-0 content align-items-center">
                             <div class="row">
                                 <div class="col-lg-12">
-                                Hylocereus undarus yang lebih populer dengan sebutan white pitaya adalah buah naga yang kulitnya berwarna merah dan dagingnya berwarna putih. Warna merah buah ini sangat kontras dengan warna daging buah. Buah naga putih memiliki kandungan fenol total sebesar 28,65±1,79 mg gallic acid /100g buah, lebih besar dibandingkan dengan buah naga merah yang hanya 24,22±0,95 mg gallic acid/100g buah (Choo & Yong 2011). Pada kulit buah terdapat sisik atau jumpai berwana hijau. Di daram buah terdapat banyak biji benrwarna hitam. Berat buah rata-rata 400- 500g, bahkan ada yang dapat mencapai 650 g. rasa buahnya masam bercampur manis, tanaman ini lebih hanyak dikembangkan di Negara-negara produsen utama buah naga dibanding jenis lainnya karena buahnya cenderung lebih banyak diekspor. 
-
+                                    <p class="text-justify"> Hylocereus undarus yang lebih populer dengan sebutan white pitaya adalah buah naga yang kulitnya berwarna merah dan dagingnya berwarna putih. Warna merah buah ini sangat kontras dengan warna daging buah. Buah naga putih memiliki kandungan fenol total sebesar 28,65±1,79 mg gallic acid /100g buah, lebih besar dibandingkan dengan buah naga merah yang hanya 24,22±0,95 mg gallic acid/100g buah (Choo & Yong 2011). Pada kulit buah terdapat sisik atau jumpai berwana hijau. Di daram buah terdapat banyak biji benrwarna hitam. Berat buah rata-rata 400- 500g, bahkan ada yang dapat mencapai 650 g. rasa buahnya masam bercampur manis, tanaman ini lebih hanyak dikembangkan di Negara-negara produsen utama buah naga dibanding jenis lainnya karena buahnya cenderung lebih banyak diekspor.
+                                    </p>
                                 </div>
                             </div>
+                            
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12 pt-4 pt-lg-0 content align-items-center">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <p class="text-justify">Buah naga dihasilkan oleh tanaman sejenis kaktus sehingga termasuk
+                                        dalam keluarga Cactaceae dan subfamili Hylocercanea, dalam subfamili ini
+                                        terdapat beberapa genus, sedang buah naga ini termasuk dalam genus Hylocereus
+                                        (Grenus ini pun terdiri dari sekitar 16 spesies. Dua di antaranya memiliki buah:
+                                        yang Komersial, yaitu Hplocereus undatus (berdaging putih) dan Hjlocereus
+                                        costaricensis (berdaging merah). Adapun klasifikasinya “sebagai berikut
+                                        (Kristanto, 2008).</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <ul class="list-unstyled">
+                                        <li class="pb-2"><strong>Divisi: </strong>Spermatophyta</li>
+                                        <li class="pb-2"><strong>Subdivisi: </strong>Angiospermae</li>
+                                        <li class="pb-2"><strong>Kelas: </strong>Dicotyledonae</li>
+                                        <li class="pb-2"><strong>Famili: </strong>Cactaeeae</li>
+                                        <li class="pb-2"><strong>Subfamili : </strong>Hylocereanea</li>
+                                        <li class="pb-2"><strong>Genus : </strong>Hylocereus</li>
+                                        <li class="pb-2"><strong>Spesies : </strong>Hplocereus undatus (berdaging putih)</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </div>
