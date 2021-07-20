cdc5969d773b1a546eb74e7df473c810e3b091c4
Ryan Hartadi <Ryanhartadi06@gmail.com>
2021-07-20 21:17:05 +0700

    Update BuahNagaPutih.php

--- application/views/Admin/BuahNagaPutih.php
+++ application/views/Admin/BuahNagaPutih.php
@@ -13,10 +13,32 @@
                         <div class="col-lg-7 pt-4 pt-lg-0 content align-items-center">
                             <div class="row">
                                 <div class="col-lg-12">
-                                Hylocereus undarus yang lebih populer dengan sebutan white pitaya adalah buah naga yang kulitnya berwarna merah dan dagingnya berwarna putih. Warna merah buah ini sangat kontras dengan warna daging buah. Buah naga putih memiliki kandungan fenol total sebesar 28,65±1,79 mg gallic acid /100g buah, lebih besar dibandingkan dengan buah naga merah yang hanya 24,22±0,95 mg gallic acid/100g buah (Choo & Yong 2011). Pada kulit buah terdapat sisik atau jumpai berwana hijau. Di daram buah terdapat banyak biji benrwarna hitam. Berat buah rata-rata 400- 500g, bahkan ada yang dapat mencapai 650 g. rasa buahnya masam bercampur manis, tanaman ini lebih hanyak dikembangkan di Negara-negara produsen utama buah naga dibanding jenis lainnya karena buahnya cenderung lebih banyak diekspor. 
-
+                                    Hylocereus undarus yang lebih populer dengan sebutan white pitaya adalah buah naga yang kulitnya berwarna merah dan dagingnya berwarna putih. Warna merah buah ini sangat kontras dengan warna daging buah. Buah naga putih memiliki kandungan fenol total sebesar 28,65±1,79 mg gallic acid /100g buah, lebih besar dibandingkan dengan buah naga merah yang hanya 24,22±0,95 mg gallic acid/100g buah (Choo & Yong 2011). Pada kulit buah terdapat sisik atau jumpai berwana hijau. Di daram buah terdapat banyak biji benrwarna hitam. Berat buah rata-rata 400- 500g, bahkan ada yang dapat mencapai 650 g. rasa buahnya masam bercampur manis, tanaman ini lebih hanyak dikembangkan di Negara-negara produsen utama buah naga dibanding jenis lainnya karena buahnya cenderung lebih banyak diekspor.
                                 </div>
                             </div>
+                            <br>
+                            <b>Klasifikasi Buah Naga</b>
+                            <br>
+                            <div class="row mt-3">
+                                <div class="col-lg-6">
+                                    <ul class="list-unstyled">
+                                        <li class="pb-2"><strong>Kingdom: </strong> Plantae</li>
+                                        <li class="pb-2"><strong>Divisi : </strong>Spermatophyta</li>
+                                        <li class="pb-2"><strong>Subdivisi : </strong> Angiospermae</li>
+                                        <li class="pb-2"><strong>Kelas : </strong>Dicotyledonae</li>
+                                    </ul>
+                                </div>
+                                <div class="col-lg-6">
+                                    <ul class="list-unstyled">
+                                        <li class="pb-2"><strong>Ordo : </strong> Cactales</li>
+                                        <li class="pb-2"><strong>Famili : </strong> Cactaceae</li>
+                                        <li class="pb-2"><strong>Genus : </strong> Hylocereus </li>
+                                        <li class="pb-2"><strong>Spesies : </strong> Hylocereus cistericensi, Hylocereus undarus, Hylocereus polyrhizus, selenicereus megalanthus </li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
